a0dc3e3dec8b9c64bf29d2f80a41d3fc5920448a
LORDyyyyy <mlord6271@gmail.com>
2023-12-11 14:55:24 +0200

    added main

---
+++ Main.java
@@ -0,0 +1,135 @@
+public class Main {
+    public static void main(String[] args) throws Exception
+    {
+        FileStorage fs = new FileStorage();
+        Helpers hlp = new Helpers();
+
+
+        // fs.createTable("task", "id", "task_id");
+        fs.reload("task");
+        fs.createTable("projects","project_id","project_title","project_description","project_startTime","project_endtime");
+        System.out.println("file has successfulyy created");
+// addeProject("try", "a try", "1/11", "2/11", 6);
+
+        // System.out.println("." + hlp.printCell("id", 16) + ".");
+
+        // System.out.println((1.0f / 0));
+
+
+        // Get a record from a table, specify the columns to check, and the values to compare
+        
+
+        // Get all record from a table
+        // String[][] table = fs.read("emp");
+
+        // Get the header of a table.
+        // System.out.println(fs.readHeader("emp.txt")[0]);
+        // System.out.println(fs.readHeader("emp.txt")[1]);
+        
+        // if (Helpers.isEmptyTable(res))
+        //     System.out.println("its empty!!!");
+
+
+
+        // String[][] res = fs.read("emp.txt", temp, Helpers.paramsToArr("man"));
+        // for (String[] row : res)
+        // {
+        //     for (String value : row) {
+        //         System.out.print("(" + value + ")" + " \t    | ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // System.out.println("---------");
+
+        // res = fs.read("emp.txt", temp, Helpers.paramsToArr("man"), true);
+        // for (String[] row : res)
+        // {
+        //     for (String value : row) {
+        //         System.out.print("(" + value + ")" + " \t    | ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // System.out.println("-----------------");
+
+
+        // if (fs.delete("emp.txt", temp, Helpers.paramsToArr("khalid")) == 1)
+        // System.out.println("error!");
+        
+
+        // fs.add("emp", hlp.paramsToArr(fs.getNextID("emp"), "ali", "password122155"));
+        // String[][] res = fs.read("emp.txt", temp, Helpers.paramsToArr("khalid"));
+        // int[] temp = new int[] {0, 1 };
+        // int[] temp2 = new int[] {1, 2};
+        // if (fs.delete("emp", temp, Helpers.paramsToArr("ahmed", "ALIII")) == 1)
+        //     System.out.println("error!");
+
+        // fs.add("emp", hlp.paramsToArr(1, "ahmed", "sadsa"));
+
+        // SELECT * FROM emp WHERE column[0] = 5
+        // String[][] res = fs.read("emp", hlp.intToArr(0), hlp.paramsToArr(5));
+
+        // fs.update("emp",
+        //         hlp.intToArr(1), hlp.paramsToArr(881),
+        //         hlp.intToArr(1), hlp.paramsToArr("Ali"));
+
+
+        // fs.createTable("tmp", "id", "name", "password", "email", "date");
+
+
+        // fs.delete("emp", hlp.intToArr(0),
+
+        // hlp.paramsToArr(889, "dsas", true, false));
+
+        // String[][] res = fs.read("emp",
+        //                                     hlp.intToArr(1, 2),
+        //                                     hlp.paramsToArr("ali", "newPass"));
+        // if (hlp.isEmptyTable(res))
+        //     System.out.println("acc not found");
+        // else
+        //     System.out.println("Login done");
+
+
+        // String[][] res = fs.read(
+        //         "emp",
+        //         hlp.intToArr(1),
+        //         hlp.paramsToArr("ahmed"));
+
+
+
+
+        // fs.delete("emp", hlp.intToArr(0), hlp.paramsToArr(14));
+
+
+        // UPDATE emp SET name = ahmed AND password = newpass WHERE ID = 3 ;
+        //                  columns id index = 0
+        //                  column name index = 1
+        //                  column password index = 2
+        // fs.update("emp",
+        //             hlp.intToArr(0),
+        //             hlp.paramsToArr(15),
+        //             hlp.intToArr(2),
+        //             hlp.paramsToArr( "newpass")
+        //         );
+
+
+
+
+        // System.out.println();
+        // fs.add("emp", hlp.paramsToArr(fs.getNextID("emp"), "ali mohammed", "newPass"));
+        // String[][] res = fs.read("emp",  hlp.intToArr(1), hlp.paramsToArr("khalid"));
+
+        // for (String[] row : res)
+        // {
+        //     for (String value : row)
+        //     {
+        //         System.out.print("(" + value + ")" + " \t    | ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // System.out.println(fs.toString()); // TODO
+    }
+    
+}
