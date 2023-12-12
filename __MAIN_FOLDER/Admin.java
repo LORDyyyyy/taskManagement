62a2aedb51a9aef912314013f1164a481e56d945
LORDyyyyy <mlord6271@gmail.com>
2023-12-12 16:30:57 +0200

    Added main folder

---
+++ __MAIN_FOLDER/Admin.java
@@ -0,0 +1,48 @@
+public class Admin extends Person {
+
+    public Admin(String name, String password) 
+    {
+        super(name, password, "admin");
+    }
+
+    public boolean add(String Name, String password, String type) throws Exception
+    {
+        if (type.equals("leader"))
+        {
+            Leader lead = new Leader(Name, password);
+            fs.add(lead.table_name, hlp.paramsToArr(lead.getId(), Name, password));
+            return (true);
+        }
+        else if (type.equals("emp"))
+        {
+            Employee emp = new Employee(Name, password);
+            fs.add(emp.table_name, hlp.paramsToArr(emp.getId(), Name, password));
+            return (true);
+        }
+
+        return (false);
+    }
+
+    public boolean update(int ID, String name, String nameFile) throws Exception {
+        String[][] res = fs.read(nameFile, hlp.intToArr(0), hlp.paramsToArr(ID));
+        for (String[] row : res) {
+            if (Integer.parseInt(row[0]) == ID) {
+                setName(name);
+                fs.update(nameFile, hlp.intToArr(0), hlp.paramsToArr(ID), hlp.intToArr(1), hlp.paramsToArr(name));
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean delete(int ID, String nameFile) throws Exception {
+        String[][] res = fs.read(nameFile, hlp.intToArr(0), hlp.paramsToArr(ID));
+        for (String[] row : res) {
+            if (Integer.parseInt(row[0]) == ID) {
+                fs.delete(nameFile, hlp.intToArr(0), hlp.paramsToArr(ID));
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ __MAIN_FOLDER/Employee.java
@@ -0,0 +1,7 @@
+public class Employee extends Person 
+{
+    public Employee(String name, String password) 
+    {
+        super(name, password, "emp");
+    }
+}
